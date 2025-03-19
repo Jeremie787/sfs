f8ccbbbb3cb73c16c088e35f721a9e8725ba4173
Do Ngoc Minh <minhdn@rikkeisoft.com>
2025-03-19 17:33:42 +0700

    add simpleform

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -5,7 +5,23 @@ module.exports = {
     './app/views/**/*',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#003459', 
+        secondary: '#F7DBA7', 
+        neutral_100: '#00171F', 
+        neutral_80: '#242B33', 
+        neutral_60: '#667479', 
+        neutral_40: '#99A2A5', 
+        neutral_20: '#CCD1D2', 
+        neutral_10: '#EBEEEF', 
+        neutral_00: '#FDFDFD', 
+        pink_red: '#FF564F',
+        green_light: '#34C759',
+        orange_shine: '#FF912C',
+        blue_sea: '#00A7E7',
+      },
+    },
   },
   plugins: [],
 }
